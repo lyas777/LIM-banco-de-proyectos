799bc4d70b2149486a15d9b04743ef0afd479b0c
lyascat <lyascat@gmail.com>
2020-04-24 00:53:02 -0500

    configuración de boleriplatey configuración de sass

---
+++ src/components/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Home = () => {
+  // const [flipped , setFlipped] = useState([]);
+  // const handleClick = (id) => setFlipped([...flipped, id]);
+
+    return(
+      <main>
+        <header>
+
+        </header>
+
+      </main>
+    );
+
+}
+
+export default Home;
